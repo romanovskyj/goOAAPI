23986fcf94db02be11f9ee2faf2bb14d820e93f8
Eduard Romanovskyj <eromanovskyj@odin.com>
2018-01-03 13:59:55 +0400

    Test package

---
+++ README.md
@@ -0,0 +1,1 @@
+## Odin Automation API library
---
+++ client.go
@@ -0,0 +1,5 @@
+package goOAAPI
+
+func GetClient(hostname string, username string, password string, ssl bool, port int) (string, error) {
+	return "success!", nil
+}
