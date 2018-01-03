1ce30d3642575891fd5951fffdbade4d9a4af6ec
Eduard Romanovskyj <eromanovskyj@odin.com>
2018-01-03 15:06:17 +0400

    First method sample

--- client.go
+++ client.go
@@ -1,5 +1,32 @@
 package goOAAPI
 
-func GetClient(hostname string, username string, password string, ssl bool, port int) (string, error) {
-	return "success!", nil
+import "github.com/kolo/xmlrpc"
+
+type OA struct{
+	Hostname string
+	Username string
+}
+
+type Result struct{
+	ErrorMessage string `xmlrpc:"error_message"`
+}
+
+type RequestParams struct{
+	subscription_id int
+	service_template_id int
+}
+
+func (oa *OA) ActivateSubscription() Result {
+	result := Result{}
+
+	client, _ := xmlrpc.NewClient(oa.Hostname, nil)
+	client.Call("pem.activateSubscription", RequestParams{3,4}, &result)
+
+	return result
+}
+
+func GetClient(hostname string, username string, password string, ssl bool, port int) (*OA, error) {
+	oa := OA{hostname, username}
+
+	return &oa, nil
 }
